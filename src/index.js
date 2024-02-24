bd2c1b33ab8d7decda9c131847a9321e91e11ebc
Swaraj Kaushik <swarajkaushik@gmail.com>
2024-02-24 21:56:23 +0530

    upates

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const setupAndStartServer = () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
-  app.use("/api", apiRoutes);
+  app.use("/bookingservice/api", apiRoutes);
 
   app.listen(PORT, () => {
     console.log(`Server started at port: ${PORT}`);
